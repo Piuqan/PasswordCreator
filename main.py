0b88a2825661db869788b28746aca52bec63370f
Piuqan <35349907+Piuqan@users.noreply.github.com>
2019-11-21 22:24:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #program generujacy hasło o długości podanej przez użytkownika
 import random
 
-def generacjahasla(znaki):
+def generacjahasla(znaki,numeryczne,specjalne):
     znakiWielkie = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     znakiMale = "abcdefghijklmnopqrstuvwxyz"
     znakiNumeryczne = "0123456789"
     znakiSpecjalne = "!@#$%&*()[]{}"
-    znakiWszystkie = znakiMale + znakiWielkie + znakiSpecjalne + znakiNumeryczne #lista wszystkich znaków
+    znakiWszystkie = znakiMale + znakiWielkie
+    if numeryczne == "T":
+        znakiWszystkie += znakiNumeryczne
+    if specjalne == "T":
+        znakiWszystkie += znakiSpecjalne
     haslo = ''
     for x in range(znaki): #tworzenie hasła
       haslo += random.choice(znakiWszystkie) #wybór losowego znaku do hasła
@@ -15,6 +19,8 @@ def generacjahasla(znaki):
 
 try:
     znaki = int(input("Podaj liczbe znaków w haśle"))
-    generacjahasla(znaki)
+    numeryczne = input("Czy chcesz żeby użyć znaków numerycznych? T/N")
+    specjalne = input("Czy chcesz żeby użyć znaków specjalnych? T/N")
+    generacjahasla(znaki, numeryczne, specjalne)
 except:
     print("coś się spierdoliło")
